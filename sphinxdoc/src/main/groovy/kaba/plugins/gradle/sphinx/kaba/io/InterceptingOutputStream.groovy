82231d4512f3cd511fe8b73e2e55b6e56e53a578
Ryo TANAKA <ryotan@ucb-jp.net>
2013-10-10 23:20:55 +0900

    Javadocを書いてみた。

--- sphinxdoc/src/main/groovy/kaba/plugins/gradle/sphinx/kaba/io/InterceptingOutputStream.groovy
+++ sphinxdoc/src/main/groovy/kaba/plugins/gradle/sphinx/kaba/io/InterceptingOutputStream.groovy
@@ -14,9 +14,9 @@ class InterceptingOutputStream extends FilterOutputStream {
      * Creates an output stream filter built on top of the specified
      * underlying output stream.
      * <p/>
-     * When some bytes are sent to this stream, {@ #callback} is called with that bytes.
+     * When some bytes are sent to this stream, {@link #callback} is called with that bytes.
      * <p/>
-     * Then, if {@ #callback} returns {@code true}, that bytes will be sent to {@code out} too.
+     * Then, if {@link #callback} returns {@code true}, that bytes will be sent to {@code out} too.
      *
      * @param out the underlying output stream to send bytes to.
      * @param callback accepts a byte to be sent to {@code out} and returns a Boolean.
@@ -27,16 +27,47 @@ class InterceptingOutputStream extends FilterOutputStream {
         this.callback = callback
     }
 
+    /**
+     * Calls {@link #callback} with {@code b}, and when {@link #callback} returns {@code true},
+     * send {@code b} to underlying output stream.
+     *
+     * @param     b  the <code>byte</code>.
+     * @exception IOException  if an I/O error occurs.
+     * @see       FilterOutputStream#write(int)
+     */
     @Override
     void write(int b) throws IOException {
         super.write(b)
     }
 
+    /**
+     * Calls {@link #callback} with {@code b}, and when {@link #callback} returns {@code true},
+     * send {@code b} to underlying output stream.
+     *
+     * @param     b  the data to be written.
+     * @exception IOException  if an I/O error occurs.
+     * @see       FilterOutputStream#write(byte [])
+     */
     @Override
     void write(byte[] b) throws IOException {
         super.write(b)
     }
 
+    /**
+     * Calls {@link #callback} with the subarray of {@code b},
+     * and when {@link #callback} returns {@code true},
+     * send {@code b} to underlying output stream.
+     * <p/>
+     * The subarray of {@code b} is <code>len</code> bytes from the specified
+     * <code>byte</code> array starting at offset <code>off</code> to
+     * this output stream.
+     *
+     * @param      b     the data.
+     * @param      off   the start offset in the data.
+     * @param      len   the number of bytes to write.
+     * @exception  IOException  if an I/O error occurs.
+     * @see        FilterOutputStream#write(byte[], int, int)
+     */
     @Override
     void write(byte[] b, int off, int len) throws IOException {
         super.write(b, off, len)
